356143bcea13d77a96b8815960280197da10e266
anotherswsmith <stuart.smith@ntnu.no>
2018-10-05 11:30:55 +0200

    Searching for subsamples for CNP

--- Moveable exclosures/Marit.Moveable.Exploration.R
+++ Moveable exclosures/Marit.Moveable.Exploration.R
@@ -17,8 +17,8 @@ library(ggpubr)
 ################################################################
 
 # Import data seasonal biomass, dung, PRC and environmental data
-#setwd("/Users/anotherswsmith/Documents/AfricanBioServices/Data/VegSoil_GitHub/AfricanBioServices-Vegetation-and-soils/Moveable exclosures")
-OnMove<-read.csv(file="Movable.exclosure.data.csv", sep=",",header=TRUE)
+setwd("/Users/anotherswsmith/Documents/AfricanBioServices/Data/GitHubVegSoils/AfricanBioServices-Vegetation-and-soils-master/Moveable exclosures")
+OnMove<-read.csv(file="Movable.exclosure.data.FULL.csv", sep=";",header=TRUE)
 
 # Explore dataset
 names(OnMove)
@@ -30,8 +30,12 @@ dim(OnMove)
 names(OnMove)
 OnMove$site.name
 # Count function
-OnMove$harvest
-Total.NP<-count(OnMove, c("site.name","harvest")) # Total - 20, 48 etc.
+length(OnMove$harvest)
+length(OnMove$site.name)
+#Total.NP<-count(OnMove, c("site.name","harvest")) # Total - 20, 48 etc.
+
+Total.NP<-table(OnMove$harvest,OnMove$site.name)
+Total.NP<-as.data.frame(Total.NP)
 
 # Count whether missing or not
 TargetNP<-table(OnMove$harvest,OnMove$site.name, !is.na(OnMove$N.target))
@@ -46,14 +50,14 @@ OtherNP$plant.fx<-"other"
 TotalNP<-rbind(TargetNP,OtherNP)
 colnames(TotalNP)<-c("harvest","site.name","absence","freq","plant.fx")
 levels(TotalNP$absence)<-c("Absence","Prescence")
-TotalNP$freq.total<-Total.NP$freq
+TotalNP$freq.total<-Total.NP$Freq
 
 TotalNP$prop<-TotalNP$freq/TotalNP$freq.total*100
 
 TargetAb<-TotalNP[TotalNP$absence=="Absence" & TotalNP$plant.fx=="target",]
 TargetAbPlot<-ggplot(TargetAb,aes(x=harvest,y=prop, colour=site.name))+
   ylab("Missing target prop")+ggtitle("Missing target N and P")+
-geom_jitter(stat="identity", size=5, fill="white", shape=21, stroke=1, show.legend=F) +theme_bw()
+  geom_jitter(stat="identity", size=5, fill="white", shape=21, stroke=1, show.legend=F) +theme_bw()
 
 
 OtherAb<-TotalNP[TotalNP$absence=="Absence" & TotalNP$plant.fx=="other",]
@@ -68,3 +72,44 @@ egg::ggarrange(TargetAbPlot,OtherAbPlot, ncol=2)
 #ggsave("/Users/anotherswsmith/Documents/AfricanBioServices/Data/VegSoil_GitHub/AfricanBioServices-Vegetation-and-soils/Moveable exclosures/MissingNP.jpeg",
 #       width= 18, height = 12,units ="cm",
 #       dpi = 600, limitsize = TRUE)
+
+# Nicer way to do this with dplyer
+NtargetSummary<-OnMove  %>% group_by (harvest) %>%
+  summarise(harvest, count(N.target))
+
+summarise(total.count=n(),count=sum(is.na(N.target)))
+
+# How many subsamples are at SUA/NTNU
+count(OnMove$Sub.SUA)
+class(OnMove$Sub.NTNU)
+SubsamplesSUA<-OnMove[!is.na(OnMove$Sub.SUA), ] # Remove NAs
+SubsamplesSUA<-droplevels(Subsamples)
+dim(SubsamplesSUA$Sub.SUA)# 226
+
+SubsamplesNTNU<-OnMove[!is.na(OnMove$Sub.NTNU), ] # Remove NAs
+SubsamplesNTNU<-droplevels(SubsamplesNTNU)
+dim(SubsamplesNTNU$Sub.NTNU)# 138
+
+# Target NTNU and SUA
+Sub2Institues<-OnMove[OnMove$Sub.SUA>.1 & OnMove$Sub.NTNU>.1,]
+Sub2Institues<-subset(OnMove, Sub.SUA >.1 | Sub.NTNU>.1) 
+Sub2Institues<-Sub2Institues[!is.na(Sub2Institues$Sub.NTNU), ]
+Sub2Institues<-Sub2Institues[!is.na(Sub2Institues$Sub.SUA), ]
+dim(Sub2Institues)  # 70 target shared
+SharedMissingN<-Sub2Institues[is.na(Sub2Institues$N.target), ]
+dim(SharedMissingN) # 9 target
+
+# Non-target (other) NTNU and SUA
+Sub2Institues1<-OnMove[OnMove$Sub.SUA.1>.1 & OnMove$Sub.NTNU.1>.1,]
+Sub2Institues1<-subset(OnMove, Sub.SUA.1 >.1 | Sub.NTNU.1>.1) 
+Sub2Institues1<-Sub2Institues1[!is.na(Sub2Institues1$Sub.NTNU.1), ]
+Sub2Institues1<-Sub2Institues1[!is.na(Sub2Institues1$Sub.SUA.1), ]
+dim(Sub2Institues1)  # 119 non-target shared 
+
+SharedMissingNother<-Sub2Institues1[is.na(Sub2Institues1$N.other), ]
+dim(SharedMissingNother) # 18 non-target
+
+
+MissingSamplesSHARED<-rbind(SharedMissingN,SharedMissingNother)
+# Export missing samples
+write.table(MissingSamplesSHARED, file = "MissingSamplesSHARED.csv",row.names=T, na="",col.names=T, sep=";")
