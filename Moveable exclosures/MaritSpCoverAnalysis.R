cecc6df7c3b1b5ba16aca526308035b9ff01ac49
maritarneberg <marit.arneberg@gmail.com>
2019-03-20 13:54:17 +0100

    Git random changes

--- Moveable exclosures/MaritSpCoverAnalysis.R
+++
@@ -1,7 +0,0 @@
-#### Analysing species cover data ####
-#Importing data
-Species_cover_setup <- read_delim("Moveable exclosures/Species.cover.setup.csv", 
-                                  ";", escape_double = FALSE, trim_ws = TRUE)
-Species_cover_harvest <- read_delim("Moveable exclosures/Species.cover.harvest.csv", 
-                                  ";", escape_double = FALSE, trim_ws = TRUE)
-
--- Termites/Precipitation data/Rainfall_README.txt
+++
@@ -1,2 +0,0 @@
-Rainfall data is the avarage rainfall during the incubation period.
-Each rainfall data is for one plot at each block A1, W1 etc.
