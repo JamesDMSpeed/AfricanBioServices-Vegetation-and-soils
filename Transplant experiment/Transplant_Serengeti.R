57656556460f426635a8c55ab2a4951ea69f43ec
anotherswsmith <stuart.smith@ntnu.no>
2019-05-03 15:00:30 +0200

    Transplant experiment preliminary analysis

--- Transplant experiment/Transplant_Serengeti.R
+++ Transplant experiment/Transplant_Serengeti.R
@@ -86,31 +86,72 @@ dim(TP0)
 #### Cover change with and without controls ####
 
 names(TP2Con)
-TP2Con$CoverChange<-TP2Con$FilTagspCover-TP2Con$InitialTagSpcover
+# Target species cover - with & without controls
 TP2$CoverChange<-TP2$FilTagspCover-TP2$InitialTagSpcover
+TP2Con$CoverChange<-TP2Con$FilTagspCover-TP2Con$InitialTagSpcover
 
+# Summary of cover change with and without controls
 CC<-aggregate(CoverChange~fTagsp+fLanduse,TP2,mean)
+CCsd<-aggregate(CoverChange~fTagsp+fLanduse,TP2,sd)
+CC$sd<-CCsd$CoverChange
 CCcon<-aggregate(CoverChange~fTagsp+fLanduse,TP2Con,mean)
+CCconsd<-aggregate(CoverChange~fTagsp+fLanduse,TP2Con,sd)
+CCcon$sd<-CCconsd$CoverChange
+CCcon$control<-"no control"
+CC$control<-"with control"
+CC<-rbind(CC,CCcon)
 
-CC
+# Change cover 
+CCbiom<-ggplot(CC,aes(y=CoverChange, x=fTagsp, colour=control, fill=control))
+CCbiom<-CCbiom+geom_abline(intercept=0, slope=0, colour="black", linetype="dashed")
+CCbiom<-CCbiom+geom_errorbar(aes(ymin=CoverChange-sd, ymax=CoverChange+sd),position=position_dodge(width=.6),width=.2,lwd=1.1, alpha=.95,show.legend=F)
+CCbiom<-CCbiom+geom_point(position=position_dodge(width=.6),size=4.5)
+CCbiom<-CCbiom+facet_wrap(~fLanduse)
+CCbiom<-CCbiom+theme_classic()
+CCbiom
+
+# Summary of Final species cover
+CCFin<-aggregate(FilVegCover~fTagsp+fLanduse,TP2,mean)
+CCFinsd<-aggregate(FilVegCover~fTagsp+fLanduse,TP2,sd)
+CCFin$sd<-CCFinsd$FilVegCover
+CCFincon<-aggregate(FilVegCover~fTagsp+fLanduse,TP2Con,mean)
+CCFinconsd<-aggregate(FilVegCover~fTagsp+fLanduse,TP2Con,sd)
+CCFincon$sd<-CCFinconsd$FilVegCover
+CCFincon$control<-"no control"
+CCFin$control<-"with control"
+CCFin<-rbind(CCFin,CCFincon)
+
+# Final cover
+CCbiomF<-ggplot(CCFin,aes(y=FilVegCover, x=fTagsp, colour=control, fill=control))
+CCbiomF<-CCbiomF+geom_errorbar(aes(ymin=FilVegCover-sd, ymax=FilVegCover+sd),position=position_dodge(width=.6),width=.2,lwd=1.1, alpha=.95,show.legend=F)
+CCbiomF<-CCbiomF+geom_point(position=position_dodge(width=.6),size=4.5)
+CCbiomF<-CCbiomF+facet_wrap(~fLanduse)
+CCbiomF<-CCbiomF+theme_classic()
+CCbiomF
 
 ########################################################################
 #### Model probability of occurence ####
-TPprob<- glmmadmb(FinalPresAb~fTagsp+fLanduse+fTagsp:fLanduse+(1|fSite/fBlock), 
+names(TP2)
+TP2$FinalPresAb
+TPprob<- glmmadmb(FinalPresAb~fTagsp+fLanduse+Treatment+fTagsp:fLanduse+
+                    #Treatment:fTagsp
+                     (1|fSite/fBlock), 
                         family="binomial",#zeroInflation = TRUE,
                         #admb.opts=admbControl(shess=FALSE,noinit=FALSE),
+                          admb.opts=admbControl(shess=FALSE,noinit=FALSE, impSamp=200,maxfn=500,imaxfn=500,maxph=5),
                         data=TP2)
 summary(TPprob) # No difference in probabilty of species 
-drop1(TPprob,test="Chi")
+#drop1(TPprob,test="Chi")
 summary(glht(TPprob, mcp(fTagsp="Tukey"))) 
 
 #Model matrix
 MyData <- expand.grid(fTagsp = levels(TP2$fTagsp),
-                      fLanduse = levels(TP2$fLanduse))
+                      fLanduse = levels(TP2$fLanduse),
+                      Treatment = levels(TP2$Treatment))
 head(MyData)
 
 #Convert the covariate values into an X matrix
-Xp <- model.matrix(~ fTagsp+fLanduse+fTagsp:fLanduse, data = MyData)
+Xp <- model.matrix(~ fTagsp+fLanduse+Treatment+fTagsp:fLanduse, data = MyData)
 
 #Extract parameters and parameter covariance matrix
 betas    <- fixef(TPprob)
@@ -119,7 +160,6 @@ Covbetas <- vcov(TPprob)
 #Calculate the fitted values in the predictor scale
 MyData$eta <- Xp %*% betas
 MyData$Pi  <- exp(MyData$eta) / (1 + exp(MyData$eta))
-
 
 #Calculate the SEs on the scale of the predictor function
 MyData$se    <- sqrt(diag(Xp %*% Covbetas %*% t(Xp)))
@@ -140,7 +180,8 @@ MyData
 
 #### Determine species differences in biomass ####
 names(TP2)
-TPbio<- lmer(Target.weight.g~fTagsp+fLanduse+fTagsp:fLanduse+
+TPbio<- lmer(Target.weight.g~fTagsp+Treatment+fLanduse+fTagsp:fLanduse+
+               fTagsp:Treatment+
                  (1|fSite/fBlock),REML=T,
                   data=TP2)
 anova(TPbio)
@@ -152,17 +193,19 @@ summary(glht(TPbio, mcp(fTagsp="Tukey")))
 
 #### Graph biomass and probability ####
 
-MeanBio<-aggregate(Target.weight.g~fTagsp+fLanduse,TP2,mean)
-MeanBio2<-aggregate(Target.weight.g~fTagsp+fLanduse,TP2,sd)
+MeanBio<-aggregate(Target.weight.g~fTagsp+fLanduse+Treatment,TP2,mean)
+MeanBio2<-aggregate(Target.weight.g~fTagsp+fLanduse+Treatment,TP2,sd)
 MeanBio$Sd<-MeanBio2$Target.weight.g
+MyData<-MyData[!MyData$fLanduse=="Pasture" | !MyData$Treatment=="Exclosed",]
 MeanBio$Pi<-MyData$Pi
 
-TPbiom<-ggplot(MeanBio,aes(y=Target.weight.g, x=fTagsp))
+TPbiom<-ggplot(MeanBio,aes(y=Target.weight.g, x=fTagsp,colour=Treatment,fill=Treatment))
 TPbiom<-TPbiom+geom_jitter(data=TP2, fill="light grey", alpha=.5)
-TPbiom<-TPbiom+geom_point(stat="identity",aes(size=Pi),position=position_dodge(width=.6))
+TPbiom<-TPbiom+geom_point(stat="identity",aes(size=Pi,fill=Treatment))
 TPbiom<-TPbiom+geom_errorbar(aes(ymin = Target.weight.g-Sd,ymax = Target.weight.g+Sd),width=.1,show.legend=F) 
 TPbiom<-TPbiom+ylab("Biomass (g)")+xlab("Target species")
 TPbiom<-TPbiom+facet_wrap(~fLanduse)
+TPbiom<-TPbiom+scale_colour_manual(values=c("dark grey","black"))
 TPbiom<-TPbiom+scale_y_continuous(limits=c(-5,80))
 TPbiom<-TPbiom+theme_classic()
 TPbiom
@@ -176,60 +219,98 @@ with(TP3, {interaction.plot(fLanduse,fTagsp,Target.weight.g,
 bwplot(Target.weight.g~fLanduse|fTagsp,TP3)
 
 # Species differences
-summary(glht(TPcover, mcp(fTagsp="Tukey"))) 
+summary(glht(TPbio, mcp(fTagsp="Tukey"))) 
 
 # No difference in probabilty of species 
 
 #Model matrix
-MyData <- expand.grid(fTagsp = levels(TP2$fTagsp))
-head(MyData)
+MyData2 <- expand.grid(fTagsp = levels(TP2$fTagsp),
+                       fLanduse = levels(TP2$fLanduse),
+                       Treatment = levels(TP2$Treatment))
+head(MyData2)
 
 #Convert the covariate values into an X matrix
-Xp <- model.matrix(~ fTagsp, data = MyData)
+Xp2 <- model.matrix(~ fTagsp+Treatment+fLanduse+fTagsp:fLanduse+
+                      fTagsp:Treatment, data = MyData2)
 
 #Extract parameters and parameter covariance matrix
-betas    <- fixef(TPcover)
-Covbetas <- vcov(TPcover)
+betas2    <- fixef(TPbio)
+Covbetas2 <- vcov(TPbio)
 
 #Calculate the fitted values in the predictor scale
-MyData$eta <- Xp %*% betas
+MyData2$eta <- Xp %*% betas2
 
 #Calculate the SEs on the scale of the predictor function
-MyData$se    <- sqrt(diag(Xp %*% Covbetas %*% t(Xp)))
-MyData$SeUp  <- exp(MyData$eta + 1.96 *MyData$se) / 
-  (1 + exp(MyData$eta  + 1.96 *MyData$se))
-MyData$SeLo  <- exp(MyData$eta - 1.96 *MyData$se) / 
-  (1 + exp(MyData$eta  - 1.96 *MyData$se))
+MyData2$se    <- sqrt(diag(Xp2 %*% Covbetas2 %*% t(Xp2)))
+MyData2$SeUp  <- exp(MyData2$eta + 1.96 *MyData2$se) / 
+  (1 + exp(MyData2$eta  + 1.96 *MyData$se))
+MyData2$SeLo  <- exp(MyData2$eta - 1.96 *MyData2$se) / 
+  (1 + exp(MyData2$eta  - 1.96 *MyData2$se))
 
-MyData
+MyData2
 
 ########################################################################
 #### Soil properties ####
 
-TP2dig<-TP2[TP2$fTagsp=="Dig mac",]
-dim(TP2dig) # 36
-names(TP2dig)
-TP2dig$Site_block<-as.factor(with(TP2dig, paste(fSite,fBlock, sep=".")))
-BDdig<-aggregate(BD.gcm3~Site_block,TP2dig, mean)
-# 18 samples
-18*280 #= 5040 NOK...
-36*280 # 10080 NOK ...
-
+TP2c<-TP2[TP2$Trasnplant=="Control",] # Controls only...
 
 #### Soil bulk density ####
 
 BD<-aggregate(BD.gcm3~fTagsp+fLanduse,TP2, mean)
+BDc<-aggregate(BD.gcm3~fTagsp+fLanduse,TP2c, mean)
 BDSe<-aggregate(BD.gcm3~fTagsp+fLanduse,TP2, sd)
+BDcSe<-aggregate(BD.gcm3~fTagsp+fLanduse,TP2c, sd)
 BD$Sd<-BDSe$BD.gcm3
+BDc$Sd<-BDcSe$BD.gcm3
+BD$transplanted<-"trasnplanted"
+BDc$transplanted<-"control"
 
-TPBD<-ggplot(BD,aes(y=BD.gcm3, x=fTagsp))
+BD<-rbind(BD, BDc)
+
+TPBD<-ggplot(BD,aes(y=BD.gcm3, x=fLanduse, colour=transplanted, fill=transplanted))
 TPBD<-TPBD+geom_point(stat="identity", size=4,position=position_dodge(width=.6))
-TPBD<-TPBD+geom_errorbar(aes(ymin = BD.gcm3-Sd,ymax = BD.gcm3+Sd),width=.1,show.legend=F) 
+TPBD<-TPBD+geom_errorbar(aes(ymin = BD.gcm3-Sd,ymax = BD.gcm3+Sd),position=position_dodge(width=.6),width=.1,show.legend=F) 
 TPBD<-TPBD+ylab("Bulk density (g cm3)")+xlab("Target species")
-TPBD<-TPBD+facet_wrap(~fLanduse)
+TPBD<-TPBD+facet_wrap(~fTagsp)
 #TPBD<-TPBD+scale_y_continuous(limits=c(-5,80))
 TPBD<-TPBD+theme_classic()
 TPBD
+
+
+#### Ph ####
+
+pH<-aggregate(pH~fTagsp+fLanduse,TP2, mean)
+pHc<-aggregate(pH~fTagsp+fLanduse,TP2c, mean)
+pHSe<-aggregate(pH~fTagsp+fLanduse,TP2, sd)
+pHcSe<-aggregate(pH~fTagsp+fLanduse,TP2c, sd)
+pH$Sd<-pHSe$pH
+pHc$Sd<-pHcSe$pH
+pH$transplanted<-"trasnplanted"
+pHc$transplanted<-"control"
+
+pH<-rbind(pH, pHc)
+
+TPpH<-ggplot(pH,aes(y=pH, x=fLanduse, colour=transplanted, fill=transplanted))
+TPpH<-TPpH+geom_point(stat="identity", size=4,position=position_dodge(width=.6))
+TPpH<-TPpH+geom_errorbar(aes(ymin = pH-Sd,ymax = pH+Sd),position=position_dodge(width=.6),width=.1,show.legend=F) 
+TPpH<-TPpH+ylab("pH")+xlab("Target species")
+TPpH<-TPpH+facet_wrap(~fTagsp)
+#TPBD<-TPBD+scale_y_continuous(limits=c(-5,80))
+TPpH<-TPpH+theme_classic()
+TPpH
+
+#### Selecting Dig mac only ####
+#### Select controls ####
+TP2dig<-TP2[TP2$fTagsp=="Dig mac",]
+TP2c<-TP2[TP2$Trasnplant=="Control",]
+dim(TP2dig) # 36
+dim(TP2c) # 21
+NminSelect<-rbind(TP2dig,TP2c) 
+dim(NminSelect)
+TP2dig$Site_block<-as.factor(with(TP2dig, paste(fSite,fBlock, sep=".")))
+BDdig<-aggregate(BD.gcm3~Site_block,TP2dig, mean)
+57*280 # 15960 NOK ...
+write.csv(NminSelect, "Transplant experiment/NminSelect.csv",row.names=F) #,sep = ",",dec = ".",col.names = TRUE, row.names=F)
 
 
 ########################################################################
