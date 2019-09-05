7f6f1fe7c54a2296bdcc508af36d9e5b2394a72c
Stuart Smith <36596567+anotherswsmith@users.noreply.github.com>
2019-09-05 11:34:41 +0200

    Function transparent bounding box for maps

---
+++ Termites/Mapping/MakeTransparent.R
@@ -0,0 +1,11 @@
+############################
+### Make colors transparent
+############################
+
+makeTransparent<-function(someColor, alpha=75)
+{
+  newColor<-col2rgb(someColor)
+  apply(newColor, 2, function(curcoldata){rgb(red=curcoldata[1], green=curcoldata[2],
+                                              blue=curcoldata[3],alpha=alpha, maxColorValue=255)})
+} #End function
+
